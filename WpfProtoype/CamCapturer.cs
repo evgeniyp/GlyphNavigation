e35b2ed7141e2bc13d5123dc4d7eb423d992f3cd
Evgeniy Petukhov <evgeniy.petukhov@gmail.com>
2015-10-05 19:19:11 +0300

    Initial commit

---
+++ WpfProtoype/CamCapturer.cs
@@ -0,0 +1,70 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfProtoype
+{
+    public class CamCapturer
+    {
+        private FilterInfoCollection _videoDevices;
+        private VideoCaptureDevice _videoSource;
+
+        public event Action<System.Drawing.Bitmap> OnNewFrame;
+
+        public CamCapturer()
+        {
+            _videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+        }
+
+        public string[] GetCameraNames()
+        {
+            var result = new string[_videoDevices.Count];
+            for (int i = 0; i < _videoDevices.Count; i++)
+            {
+                result[i] = _videoDevices[i].Name;
+            }
+            return result;
+        }
+
+        public void Start()
+        {
+            if (_videoSource != null && !_videoSource.IsRunning)
+            {
+                _videoSource.Start();
+            }
+        }
+
+        public void Stop()
+        {
+            if (_videoSource != null && _videoSource.IsRunning)
+            {
+                _videoSource.NewFrame -= _videoSource_NewFrame;
+                _videoSource.SignalToStop();
+                //_videoSource.WaitForStop(); // hangs
+                _videoSource = null;
+            }
+        }
+
+        public void ChangeCam(int camNumber)
+        {
+            if (camNumber > -1 && _videoDevices.Count > 0 && camNumber < _videoDevices.Count)
+            {
+                Stop();
+
+                _videoSource = new VideoCaptureDevice(_videoDevices[camNumber].MonikerString);
+                _videoSource.NewFrame += _videoSource_NewFrame;
+            }
+        }
+
+        private void _videoSource_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            if (OnNewFrame != null)
+            {
+                OnNewFrame(eventArgs.Frame);
+            }
+        }
+    }
+}
---
+++ WpfProtoype/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using AForge;
+using AForge.Vision.GlyphRecognition;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfProtoype
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CamCapturer _camCapturer;
+
+        private GlyphRecognizer _glyphRecognizer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            GlyphDatabase glyphDatabase = new GlyphDatabase(5);
+            glyphDatabase.Add(new Glyph("Glider", new byte[,] {
+                {0,0,0,0,0},
+                {0,0,1,0,0},
+                {0,1,1,0,0},
+                {0,0,1,1,0},
+                {0,0,0,0,0},
+            }));
+            _glyphRecognizer = new GlyphRecognizer(5);
+            _glyphRecognizer.GlyphDatabase = glyphDatabase;
+
+            InitializeCapturer();
+            InitializeComboBox_Webcams();
+            if (ComboBox_Webcams.Items.Count > 0)
+            {
+                ComboBox_Webcams.SelectedIndex = 0;
+            }
+        }
+
+        private void InitializeCapturer()
+        {
+            _camCapturer = new CamCapturer();
+            _camCapturer.OnNewFrame += camCapturer_NewFrame;
+        }
+
+        private void camCapturer_NewFrame(System.Drawing.Bitmap bitmap)
+        {
+
+            var glyphs = _glyphRecognizer.FindGlyphs(bitmap);
+            if (glyphs.Count > 0)
+            {
+                var glyph = glyphs[0];
+                List<IntPoint> glyphPoints = glyph.RecognizedGlyph == null ? glyph.Quadrilateral : glyph.RecognizedQuadrilateral;
+                System.Drawing.Pen pen = new System.Drawing.Pen(System.Drawing.Brushes.Red, 3);
+
+                Graphics g = Graphics.FromImage(bitmap);
+                g.DrawPolygon(pen, ToPointsArray(glyphPoints));
+                g.DrawEllipse(new System.Drawing.Pen(System.Drawing.Brushes.Green, 3), glyphPoints[0].X, glyphPoints[0].Y, 10, 10);
+            }
+
+            BitmapImage bitmapImage = BitmapToBitmapImage(bitmap);
+            UpdateImage_Frame(bitmapImage);
+        }
+
+        private BitmapImage BitmapToBitmapImage(Bitmap bitmap)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+            }
+
+            return bitmapImage;
+        }
+
+        private void UpdateImage_Frame(BitmapImage bi)
+        {
+            Image_Frame.Dispatcher.Invoke(() => { Image_Frame.Source = bi; });
+        }
+
+        private System.Drawing.Point[] ToPointsArray(List<IntPoint> points)
+        {
+            int count = points.Count;
+            System.Drawing.Point[] pointsArray = new System.Drawing.Point[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                pointsArray[i] = new System.Drawing.Point(points[i].X, points[i].Y);
+            }
+
+            return pointsArray;
+        }
+
+        private void ComboBox_Webcams_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            _camCapturer.ChangeCam(ComboBox_Webcams.SelectedIndex);
+            _camCapturer.Start();
+        }
+
+        private void InitializeComboBox_Webcams()
+        {
+            var cameraNames = _camCapturer.GetCameraNames();
+            foreach (var item in cameraNames)
+            {
+                ComboBox_Webcams.Items.Add(item);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _camCapturer.Stop();
+        }
+    }
+}
